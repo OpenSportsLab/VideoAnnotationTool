bdd76faabdbaa3082bb761ff446ca95fb3d549cb
SilvioGiancola <silvio.giancola@kaust.edu.sa>
2026-08-30 16:26:58 +0000

    chore: Bump version to v1.4.5

--- annotation_tool/app_info.py
+++ annotation_tool/app_info.py
@@ -3,7 +3,7 @@
 from media_control_settings import format_control_value
 
 APP_DISPLAY_NAME = "Video Annotation Tool"
-APP_VERSION = "v1.4.4"
+APP_VERSION = "v1.4.5"
 
 _SHORTCUTS_HELP_PREFIX = """Project
 Ctrl+O: Load dataset JSON
